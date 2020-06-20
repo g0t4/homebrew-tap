1884b10397f8cad31adfbb23010e561f00871833
Wes Higbee <wes.mcclure@gmail.com>
2020-06-19 21:27:50 -0400

    Add Sonos s1 and s2 app casks

---
+++ Casks/sonos-s1.rb
@@ -0,0 +1,20 @@
+cask 'sonos-s1' do
+    version '11.2'
+    sha256 '1a98300bd16bb2f727eb1921db80777cc23178a483d56b44a9bb5d95d9ea6602'
+    # 1a98300bd16bb2f727eb1921db80777cc23178a483d56b44a9bb5d95d9ea6602  SonosDesktopController112.dmg
+
+    # download page: https://support.sonos.com/s/downloads?language=en_US
+    url "https://update-software.sonos.com/software/mac/mdcr/SonosDesktopController#{version.no_dots}.dmg"
+
+    name 'Sonos S1 Controller'
+    homepage 'https://support.sonos.com/s/downloads'
+
+    auto_updates true
+
+    app 'Sonos S1 Controller.app'
+
+    # this is the release notes page (can see latest release here too)
+    appcast 'https://support.sonos.com/s/article/3521'
+
+    zap trash: '~/Library/Application Support/Sonos S1 Controller'
+end
---
+++ Casks/sonos-s2.rb
@@ -0,0 +1,21 @@
+cask 'sonos-s2' do
+    version '12.0'
+    sha256 '7dcab807a75f9faef5941f96eff3e823771187e9f6fea3892d2674f192c0a02f'
+    # 7dcab807a75f9faef5941f96eff3e823771187e9f6fea3892d2674f192c0a02f  SonosDesktopController120.dmg
+
+    # download page: https://support.sonos.com/s/downloads?language=en_US
+    url "https://update-software.sonos.com/software/mac/mdcr/SonosDesktopController#{version.no_dots}.dmg"
+
+    name 'Sonos S2'
+    homepage 'https://support.sonos.com/s/sonos-s2-overview?language=en_US'
+    # Sonos S2 compatibility: https://support.sonos.com/s/article/4786?language=en_US
+
+    auto_updates true
+
+    app 'Sonos.app'
+
+    # this is the release notes page (can see latest release here too)
+    appcast 'https://support.sonos.com/s/article/3521'
+
+    zap trash: '~/Library/Application Support/Sonos'
+end
