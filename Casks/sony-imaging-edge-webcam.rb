b4284fb278e9caaac157d75d006616ff3dcdb6a1
Wes Higbee <wes.mcclure@gmail.com>
2023-02-04 17:01:51 -0600

    fix up uninstall

--- Casks/sony-imaging-edge-webcam.rb
+++ Casks/sony-imaging-edge-webcam.rb
@@ -17,15 +17,25 @@ cask 'sony-imaging-edge-webcam' do
   url 'https://di.update.sony.net/NEX/gvKlzuWNlL/IEW110_2111a.dmg'
   homepage 'https://support.d-imaging.sony.co.jp/app/webcam/en/'
 
-  # dmg => pkg
+  # Install via embedded pkg (inside dmg)
   pkg "IEW_INST.pkg"
-  # TODO uninstall (anything special) => dmg has Uninstall.app (next to pkg installer)
+
+  # Uninstall
+  #
+  # - Can't call Uninstall.app b/c it was downloaded (would have to approve in Security)
+  #   uninstall script: {
+  #     executable: "Uninstall.app"
+  #     executable: "Uninstall.app/Contents/MacOS/Application\ Stub"
+  #   }
+  #
+  # - Instead, using pkgutil (seems to work)... found pkg ID via pkgutil --pkgs"
+  uninstall pkgutil: 'com.sony.Webcam'
 
   auto_updates false # TODO find out... given its not an app but a virtual device I don't see how there would be any sort of update daemon or interface... might be one in "Imaging Edge" software which IIGC is a superset of this webcam device driver plus camera controls and more...?
 
   appcast 'https://screencast-o-matic.com/release-notes'
 
   # formula cookbook: https://docs.brew.sh/Formula-Cookbook
-
+  # cask cookbook: https://docs.brew.sh/Cask-Cookbook
   # example cask (dmg => pkg) https://github.com/Homebrew/homebrew-cask/commit/49b2fc64931a58c6e3b099bd05081abff1f2fbf2
 end
