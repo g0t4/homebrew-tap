b4d61439b6bdc758d085e55fdca4404a9d052cc3
Wes Higbee <wes.mcclure@gmail.com>
2023-03-11 16:36:03 -0600

    notes

--- Casks/sonos-s2.rb
+++ Casks/sonos-s2.rb
@@ -8,6 +8,8 @@ cask 'sonos-s2' do
     #    FYI this link redirects to the current version, i.e. right now:
     #      https://update-software.sonos.com/software/veqbtaxi/Sonos_71.1-38240.dmg
     #      I last had version "12.0" and now it's "71.1"!? clearly this version scheme is radical and irrelevant IMO given that the app auto-updates so to install it I just need to grab latest version and then it will update itself thereafter.
+    #         LOL ok so the version is marked as 15.2.1 while the file shows 71.1?! oh sonos...
+    #           release notes: https://support.sonos.com/en/article/release-notes-for-sonos-s2
     url "https://www.sonos.com/en-us/redir/controller_software_mac2"
 
     name 'Sonos S2'
@@ -20,6 +22,7 @@ cask 'sonos-s2' do
 
     # this is the release notes page (can see latest release here too)
     appcast 'https://support.sonos.com/s/article/3521'
+    appcast 'https://support.sonos.com/en/article/release-notes-for-sonos-s2'
 
     zap trash: [
          '~/Library/Application Support/SonosV2',
